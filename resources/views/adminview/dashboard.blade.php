433147b2ca4b5d6fffd96d2d77e112253767e43f
drashtiparmar480-cpu <drashtiparmar480@gmail.com>
2025-12-27 14:30:53 +0530

    dashboard update

--- resources/views/adminview/dashboard.blade.php
+++ resources/views/adminview/dashboard.blade.php
@@ -242,9 +242,33 @@ $pagetype = 'dashboard';
                         <b>${item.first_name || ''} ${item.last_name || ''}</b>
                     </td>
                     <td>
-                    <b>${item.email || ''}</b><br>
-                    <i class="fab fa-whatsapp"></i> ${item.whatsapp_no || ''} <br>
-                    <i class="fas fa-phone"></i> ${item.phone_no || ''}
+                    <div class="tag-list">
+                                    <span class="tag tag-with-icon tag-gray">
+                                        <div class="tag-icon mr-2 pr-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
+                                                <path d="M3 7l9 6l9 -6"></path>
+                                            </svg>
+                                        </div>
+                                        ${item.email ? item.email : '-'}
+                                    </span>
+                                    <span class="tag tag-with-icon tag-gray">
+                                        <div class="tag-icon mr-2 pr-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-phone" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
+                                            </svg>
+                                        </div>
+                                        ${item.phone_no ? item.phone_no : '-'}
+                                    </span>
+                                    <span class="tag tag-with-icon tag-gray">
+                                        <div class="tag-icon mr-2 pr-2">
+                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="1.5"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-whatsapp"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" /><path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" /></svg>
+                                        </div>
+                                    ${item.whatsapp_no ? item.whatsapp_no : '-'}
+                                    </span>
+                                </div>
                         </td>
                     <td>
                         ${new Date(item.created_at).toDateString()}
