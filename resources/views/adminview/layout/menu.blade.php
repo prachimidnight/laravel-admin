fca0c9efef063d02dd1a4a3c2a55cf0a229728e9
prachimidnight <prachi@themidnight.in>
2025-12-27 15:13:48 +0530

    Update menu

--- resources/views/adminview/layout/menu.blade.php
+++ resources/views/adminview/layout/menu.blade.php
@@ -29,29 +29,29 @@
                 $userdata = Session::get('userdata');
             @endphp
             @if ($userdata)
-                <div class="menu-item">
-                    <a class="menu-link" href="dashboard">
-                        <span class="menu-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                                stroke-linejoin="round" class="icon icon-tabler icon-tabler-dashboard">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                <path d="M12 13m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                                <path d="M13.45 11.55l2.05 -2.05" />
-                                <path d="M6.4 20a9 9 0 1 1 11.2 0z" />
-                            </svg>
-                        </span>
-                        <span class="menu-title">Dashboard</span>
-                    </a>
-                </div>
+            <div class="menu-item">
+                <a class="menu-link" href="dashboard">
+                    <span class="menu-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                            stroke-linejoin="round" class="icon icon-tabler icon-tabler-dashboard">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M12 13m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                            <path d="M13.45 11.55l2.05 -2.05" />
+                            <path d="M6.4 20a9 9 0 1 1 11.2 0z" />
+                        </svg>
+                    </span>
+                    <span class="menu-title">Dashboard</span>
+                </a>
+            </div>
 
-                <div class="menu-item">
-                    <a class="menu-link with-submenu" href="guest">
+            <div class="menu-item menu-accordion">
+                    <a class="menu-link" href="javascript:void(0);">
                         <span class="menu-icon">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"
-                                stroke-linecap="round" stroke-linejoin="round"
-                                class="icon icon-tabler icons-tabler-outline icon-tabler-users">
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                                class="icon icon-tabler icon-tabler-users">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
                                 <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
@@ -59,44 +59,60 @@
                                 <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
                             </svg>
                         </span>
-                        <span class="menu-title">Guest</span>
-                        <span class="tag bg-primary white-text rounded-pill" id="guest-count">0</span>
-                        {{-- <span class="submenu-caret">▸</span> <!-- Arrow to indicate dropdown --> --}}
+
+                        <span class="menu-title">Guest Details</span>
+
+                        <span class="menu-arrow">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
+                                class="icon icon-tabler icon-tabler-chevron-down">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M6 9l6 6l6 -6" />
+                            </svg>
+                        </span>
                     </a>
-                    {{-- <div class="submenu">
-                        <div class="menu-item">
-                            <a class="menu-link" href="bulkupload">
-                                <span class="menu-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                        stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"
-                                        class="icon icon-tabler icon-tabler-upload">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                        <path d="M12 3v12m0 0l-4 -4m4 4l4 -4"/>
-                                        <path d="M4 21h16"/>
-                                    </svg>
-                                </span>
-                                <span class="menu-title">Bulk Upload</span>
-                            </a>
-                        </div>
-                    </div> --}}
+
+                  <!-- Sub Menu -->
+                <div class="menu-sub menu-sub-accordion">
+                    <div class="menu-item">
+                        <a class="menu-link" href="guest">
+                            <span class="menu-bullet">
+                                <span class="bullet bullet-dot"></span>
+                            </span>
+                            <span class="menu-title">Guest</span>
+                            <span class="tag bg-primary white-text rounded-pill" id="guest-count">0</span>
+                        </a>
+                    </div>
+
+                    <div class="menu-item">
+                        <a class="menu-link" href="bulkupload">
+                            <span class="menu-bullet">
+                                <span class="bullet bullet-dot"></span>
+                            </span>
+                            <span class="menu-title">Bulk Upload</span>
+                            <span class="tag bg-primary white-text rounded-pill" id="bulk-upload-count">0</span>
+                        </a>
+                    </div>
                 </div>
-                
+            </div>    
+
             <div class="menu-item">
-                <a class="menu-link" href="masters">
-                    <span class="menu-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"
-                            stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-cube">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <path d="M21 16.008v-8.018a1.98 1.98 0 0 0 -1 -1.717l-7 -4.008a2.016 2.016 0 0 0 -2 0l-7 4.008c-.619 .355 -1 1.01 -1 1.718v8.018c0 .709 .381 1.363 1 1.717l7 4.008a2.016 2.016 0 0 0 2 0l7 -4.008c.619 -.355 1 -1.01 1 -1.718z">
-                            </path>
-                            <path d="M12 22v-10"></path>
-                            <path d="M12 12l8.73 -5.04"></path>
-                            <path d="M3.27 6.96l8.73 5.04"></path>
-                        </svg>
-                    </span>
-                    <span class="menu-title">Masters</span>
-                </a>
+              <a class="menu-link" href="masters">
+                  <span class="menu-icon">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                          viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"
+                          stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-cube">
+                          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                          <path d="M21 16.008v-8.018a1.98 1.98 0 0 0 -1 -1.717l-7 -4.008a2.016 2.016 0 0 0 -2 0l-7 4.008c-.619 .355 -1 1.01 -1 1.718v8.018c0 .709 .381 1.363 1 1.717l7 4.008a2.016 2.016 0 0 0 2 0l7 -4.008c.619 -.355 1 -1.01 1 -1.718z">
+                          </path>
+                          <path d="M12 22v-10"></path>
+                          <path d="M12 12l8.73 -5.04"></path>
+                          <path d="M3.27 6.96l8.73 5.04"></path>
+                      </svg>
+                  </span>
+                  <span class="menu-title">Masters</span>
+              </a>
             </div>
             
             @else
